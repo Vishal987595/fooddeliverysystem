adef0ba99ad3073029859ff9350bbbe452ca81af
Vishal Ghoniya <vishal.ghoniya@iitgn.ac.in>
2023-03-16 20:36:24 +0530

    integrating cuisine backend

--- customer/views.py
+++ customer/views.py
@@ -8,7 +8,14 @@ def users():
 
 @customer.route('/dashboard')
 def dashboard():
-    return render_template("customer/dashboard.html")
+    cursor = mysql.connection.cursor()
+    cursor.execute('select distinct type  from cuisine_type')
+    output = cursor.fetchall()
+    cuisines = []
+    for i in range(len(output)):
+        p = str(output[i])
+        cuisines.append(p[2:-3])
+    return render_template("customer/dashboard.html", cuisines=cuisines)
 
 @customer.route('/userprofile')
 def userprofile():
